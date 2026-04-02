05cd5d4438e21f04d130051643820f4ccf175087
Tomasz Grodecki <tgrodecki89@gmail.com>
2026-04-02 03:47:14 +0200

    feat(models) : add TaskItem

---
+++ TaskManagerAPI/Models/TaskItem.cs
@@ -0,0 +1,10 @@
+namespace TaskManagerAPI.Models
+{
+	public class TaskItem
+	{
+		public int Id { get; set; }
+		public required string Title { get; set; }
+		public string? Description { get; set; }
+		public bool IsDone { get; set; }
+	}
+}
