57387b9dc05e75cdb797ab489361fa343f5aaf14
Tomasz Grodecki <tgrodecki89@gmail.com>
2026-04-06 22:44:09 +0200

    feat(DTOs): add TaskDto, UpdateTaskDto, UpdateTaskStatusDto

--- TaskManagerAPI/DTOs/CreateTaskDto.cs
+++ TaskManagerAPI/DTOs/CreateTaskDto.cs
@@ -1,8 +1,26 @@
-namespace TaskManagerAPI.DTOs
+namespace TaskManagerAPI.DTOs;
+
+public class TaskDto
 {
-    public class CreateTaskDto
-    {
-        public required string Title { get; set; }
-        public string? Description { get; set; }
-    }
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string? Description { get; set; }
+    public bool IsDone { get; set; }
+}
+
+public class CreateTaskDto
+{
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+}
+
+public class UpdateTaskDto
+{
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+}
+
+public class UpdateTaskStatusDto
+{
+    public bool IsDone { get; set; }
 }
