c1aa25633465009f4af1d8619a367a51cc5d2504
Tomasz Grodecki <tgrodecki89@gmail.com>
2026-04-23 23:33:10 +0200

    refactor(dtos): use required instead of null!

--- TaskManagerAPI/DTOs/TaskDtos.cs
+++ TaskManagerAPI/DTOs/TaskDtos.cs
@@ -5,7 +5,7 @@ namespace TaskManagerAPI.DTOs;
 public class TaskDto
 {
     public int Id { get; set; }
-    public string Title { get; set; } = null!;
+    public required string Title { get; set; }
     public string? Description { get; set; }
     public bool IsDone { get; set; }
 }
