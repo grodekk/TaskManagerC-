5338755176ba622e30a4fd8de83154c8508a43bf
Tomasz Grodecki <tgrodecki89@gmail.com>
2026-08-07 02:54:41 +0200

    chore(db): add migration for required task project

---
+++ TaskManagerAPI/Migrations/20260807005340_MakeTaskProjectRequired.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeTaskProjectRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Projects_ProjectId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectId",
+                table: "Tasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Projects_ProjectId",
+                table: "Tasks",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Projects_ProjectId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectId",
+                table: "Tasks",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Projects_ProjectId",
+                table: "Tasks",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id");
+        }
+    }
+}
